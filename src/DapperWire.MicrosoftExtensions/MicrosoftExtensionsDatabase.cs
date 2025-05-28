7b89fab18572d8adb3c3e7d6bc047dc70128bef2
João Simões <gravity00@users.noreply.github.com>
2025-05-28 16:31:43 +0100

    implemented façades using Microsoft extension packages

---
+++ src/DapperWire.MicrosoftExtensions/MicrosoftExtensionsDatabase.cs
@@ -0,0 +1,59 @@
+﻿using System.Data.Common;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace DapperWire;
+
+/// <summary>
+/// Represents a database connection.
+/// </summary>
+/// <param name="logger">The database logger.</param>
+/// <param name="options">The database options.</param>
+/// <param name="dbConnectionFactory">The <see cref="DbConnection"/> factory.</param>
+public class MicrosoftExtensionsDatabase(
+    ILogger<Database> logger,
+    IOptions<DatabaseOptions> options,
+    DbConnectionFactory dbConnectionFactory
+) : Database(options.Value, dbConnectionFactory)
+{
+    /// <inheritdoc />
+    protected override async Task<GetDbConnectionResult> GetDbConnectionAsync(CancellationToken ct)
+    {
+        logger.LogDebug("Getting the database connection...");
+
+        var result = await base.GetDbConnectionAsync(ct).ConfigureAwait(false);
+
+        if (result.IsNew)
+            logger.LogInformation("A new connection to the database is now open");
+
+        return result;
+    }
+}
+
+/// <summary>
+/// Represents a strongly-typed database connection.
+/// </summary>
+/// <typeparam name="TName">The database name.</typeparam>
+/// <param name="logger">The database logger.</param>
+/// <param name="options">The database options.</param>
+/// <param name="dbConnectionFactory">The <see cref="DbConnection"/> factory.</param>
+public class MicrosoftExtensionsDatabase<TName>(
+    ILogger<Database<TName>> logger,
+    IOptions<DatabaseOptions> options,
+    DbConnectionFactory<TName> dbConnectionFactory
+) : Database<TName>(options.Value, dbConnectionFactory)
+    where TName : IDatabaseName
+{
+    /// <inheritdoc />
+    protected override async Task<GetDbConnectionResult> GetDbConnectionAsync(CancellationToken ct)
+    {
+        logger.LogDebug("Getting the database connection...");
+
+        var result = await base.GetDbConnectionAsync(ct).ConfigureAwait(false);
+
+        if (result.IsNew)
+            logger.LogInformation("A new connection to the database is now open");
+
+        return result;
+    }
+}
---
+++ src/DapperWire.MicrosoftExtensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,74 @@
+﻿using System.Data.Common;
+using DapperWire;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// Provides extension methods for <see cref="IServiceCollection"/> instances.
+/// </summary>
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// Adds the database services to the service collection.
+    /// </summary>
+    /// <param name="services">The service collection.</param>
+    /// <param name="connectionFactory">The connection factory.</param>
+    /// <param name="config">An optional callback to configure database options.</param>
+    /// <returns>The service collection after changes.</returns>
+    /// <exception cref="ArgumentNullException"></exception>
+    public static IServiceCollection AddDatabase(
+        this IServiceCollection services,
+        Func<IServiceProvider, Task<DbConnection>> connectionFactory,
+        Action<DatabaseOptions>? config = null
+    )
+    {
+        if (services == null) throw new ArgumentNullException(nameof(services));
+        if (connectionFactory == null) throw new ArgumentNullException(nameof(connectionFactory));
+
+        if (config is not null)
+            services.Configure(config);
+
+        services.AddSingleton<DbConnectionFactory>(s => () => connectionFactory(s));
+        services.AddSingleton<IDatabaseFactory, MicrosoftExtensionsDatabaseFactory>();
+        services.AddScoped<IDatabase, MicrosoftExtensionsDatabase>();
+
+        return services;
+    }
+
+    /// <summary>
+    /// Adds the strongly-typed database services to the service collection.
+    /// </summary>
+    /// <typeparam name="TName"></typeparam>
+    /// <param name="services">The service collection.</param>
+    /// <param name="connectionFactory">The connection factory.</param>
+    /// <param name="config">An optional callback to configure database options.</param>
+    /// <param name="registerAsDefault">Should this database be considered the default database?</param>
+    /// <returns>The service collection after changes.</returns>
+    /// <exception cref="ArgumentNullException"></exception>
+    public static IServiceCollection AddDatabase<TName>(
+        this IServiceCollection services,
+        Func<IServiceProvider, Task<DbConnection>> connectionFactory,
+        Action<DatabaseOptions>? config = null,
+        bool registerAsDefault = false
+    ) where TName : IDatabaseName
+    {
+        if (services == null) throw new ArgumentNullException(nameof(services));
+        if (connectionFactory == null) throw new ArgumentNullException(nameof(connectionFactory));
+
+        if (config is not null)
+            services.Configure(config);
+
+        services.AddSingleton<DbConnectionFactory<TName>>(s => () => connectionFactory(s));
+        services.AddSingleton<IDatabaseFactory<TName>, MicrosoftExtensionsDatabaseFactory<TName>>();
+        services.AddScoped<IDatabase<TName>, MicrosoftExtensionsDatabase<TName>>();
+
+        if (registerAsDefault)
+        {
+            services.AddSingleton<IDatabaseFactory>(s => s.GetRequiredService<IDatabaseFactory<TName>>());
+            services.AddScoped<IDatabase>(s => s.GetRequiredService<IDatabase<TName>>());
+        }
+
+        return services;
+    }
+}
