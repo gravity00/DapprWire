ef8a59759d43e9293abc57fee253cdee7f251d79
João Simões <gravity00@users.noreply.github.com>
2025-06-03 18:48:58 +0100

    created Database.ConnectAsync tests

---
+++ test/DapperWire.Tests/Core/Databases/DatabaseConnectTests.cs
@@ -0,0 +1,35 @@
+﻿namespace DapperWire.Core.Databases;
+
+[Collection(nameof(RequireDatabase))]
+public class DatabaseConnectTests(DatabaseFixture fixture, ITestOutputHelper output)
+{
+    [Fact]
+    public async Task Connect_Succeed()
+    {
+        var database = CoreHelpers.CreateTestDatabase(output, fixture.GetDbConnection);
+
+        await using var session = await database.ConnectAsync(CancellationToken.None);
+
+        Assert.NotNull(session);
+    }
+
+    [Fact]
+    public async Task Options_OnConnectionOpen_Invoked()
+    {
+        var onConnectionOpenInvoked = false;
+        var database = CoreHelpers.CreateTestDatabase(output, fixture.GetDbConnection, options =>
+        {
+            options.OnConnectionOpen = (session, _) =>
+            {
+                Assert.NotNull(session);
+
+                onConnectionOpenInvoked = true;
+                return Task.CompletedTask;
+            };
+        });
+
+        await using var session = await database.ConnectAsync(CancellationToken.None);
+
+        Assert.True(onConnectionOpenInvoked);
+    }
+}
