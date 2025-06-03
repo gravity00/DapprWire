b49949a0711ba0c5eb6e22ef0e0ecb3f9deda1b1
João Simões <gravity00@users.noreply.github.com>
2025-06-03 10:12:28 +0100

    helper method for inicializing test instances

---
+++ test/DapperWire.Tests/Core/CoreHelpers.cs
@@ -0,0 +1,43 @@
+﻿using System.Data;
+
+namespace DapperWire.Core;
+
+public static class CoreHelpers
+{
+    public static Database CreateTestDatabase(
+        ITestOutputHelper output,
+        DbConnectionFactory dbConnectionFactory,
+        Action<DatabaseOptions>? config = null
+    )
+    {
+        var options = new DatabaseOptions
+        {
+            DefaultIsolationLevel = IsolationLevel.ReadCommitted,
+            DefaultTimeout = 5
+        };
+        config?.Invoke(options);
+        return new Database(Log, options, dbConnectionFactory);
+
+        void Log(
+            Type type,
+            DatabaseLogLevel logLevel,
+            Exception? exception,
+            string message,
+            params object?[] args
+        )
+        {
+            try
+            {
+                output.WriteLine($"[{logLevel}] {type.Name} {message}");
+                if (args.Length > 0)
+                    output.WriteLine($"  Args: {string.Join("|", args)}");
+                if (exception is not null)
+                    output.WriteLine(exception.ToString());
+            }
+            catch (InvalidOperationException)
+            {
+
+            }
+        }
+    }
+}
