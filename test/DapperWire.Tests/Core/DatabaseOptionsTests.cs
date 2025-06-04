0de151bf87c24f635a62324e87129e50fb1c1dcf
João Simões <gravity00@users.noreply.github.com>
2025-06-04 12:02:20 +0100

    added tests for DatabaseOptions

---
+++ test/DapperWire.Tests/Core/DatabaseOptionsTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Data;
+using System.Data.Common;
+
+namespace DapperWire.Core;
+
+public class DatabaseOptionsTests
+{
+    [Fact]
+    public void Constructor_DefaultValues()
+    {
+        var options = new DatabaseOptions();
+
+        Assert.Equal(IsolationLevel.ReadCommitted, options.DefaultIsolationLevel);
+        Assert.Null(options.OnConnectionOpen);
+        Assert.Null(options.DefaultTimeout);
+    }
+
+    [Fact]
+    public void Setters_GettersAreSame()
+    {
+        const IsolationLevel isolationLevel = IsolationLevel.Serializable;
+        const int defaultTimeout = 30;
+        var onConnectionOpen = new Func<DbConnection, CancellationToken, Task>((_, _) => Task.CompletedTask);
+
+        var options = new DatabaseOptions
+        {
+            DefaultIsolationLevel = isolationLevel,
+            DefaultTimeout = defaultTimeout,
+            OnConnectionOpen = onConnectionOpen,
+        };
+
+        Assert.Equal(isolationLevel, options.DefaultIsolationLevel);
+        
+        Assert.NotNull(options.OnConnectionOpen);
+        Assert.Same(onConnectionOpen, options.OnConnectionOpen);
+
+        Assert.NotNull(options.DefaultTimeout);
+        Assert.Equal(defaultTimeout, options.DefaultTimeout);
+    }
+}
