9775dd7677d15f311fd144f3d3da6d102eeca434
João Simões <gravity00@users.noreply.github.com>
2025-06-04 11:48:54 +0100

    unit tests for transactions

---
+++ test/DapperWire.Tests/Core/DatabaseTransactionTests.cs
@@ -0,0 +1,33 @@
+﻿namespace DapperWire.Core;
+
+[Collection(nameof(RequireDatabase))]
+public class DatabaseTransactionTests(DatabaseFixture fixture, ITestOutputHelper output)
+{
+    [Fact]
+    public async Task BeginTransaction_Succeed()
+    {
+        var database = CoreHelpers.CreateTestDatabase(output, fixture.GetDbConnection);
+
+        await using var session = await database.ConnectAsync(CancellationToken.None);
+
+        await using var transaction = await session.BeginTransactionAsync(CancellationToken.None);
+
+        Assert.NotNull(transaction);
+        Assert.IsType<DatabaseTransaction>(transaction);
+    }
+
+    [Fact]
+    public async Task BeginTransaction_AlreadyOpen_Fail()
+    {
+        var database = CoreHelpers.CreateTestDatabase(output, fixture.GetDbConnection);
+        
+        await using var session = await database.ConnectAsync(CancellationToken.None);
+        
+        await using var transaction = await session.BeginTransactionAsync(CancellationToken.None);
+        
+        await Assert.ThrowsAsync<InvalidOperationException>(async () =>
+        {
+            await session.BeginTransactionAsync(CancellationToken.None);
+        });
+    }
+}
