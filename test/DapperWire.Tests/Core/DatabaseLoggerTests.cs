1eac557c9b7545026ab4bcc0bba62d19fea3f992
João Simões <gravity00@users.noreply.github.com>
2025-06-04 12:49:11 +0100

    added tests for DatabaseLogger default implementation

---
+++ test/DapperWire.Tests/Core/DatabaseLoggerTests.cs
@@ -0,0 +1,39 @@
+﻿namespace DapperWire.Core;
+
+public class DatabaseLoggerTests
+{
+    [Theory]
+    [InlineData(DatabaseLogLevel.Undefined)]
+    [InlineData(DatabaseLogLevel.Debug)]
+    [InlineData(DatabaseLogLevel.Info)]
+    [InlineData(DatabaseLogLevel.Warn)]
+    [InlineData(DatabaseLogLevel.Error)]
+    public void DefaultBehavior_IsEnabled_ReturnsFalse(DatabaseLogLevel level)
+    {
+        var logger = new DatabaseLogger();
+
+        var isEnabled = logger.IsEnabled<DatabaseLoggerTests>(level);
+        Assert.False(isEnabled);
+    }
+
+    [Theory]
+    [InlineData(DatabaseLogLevel.Undefined)]
+    [InlineData(DatabaseLogLevel.Debug)]
+    [InlineData(DatabaseLogLevel.Info)]
+    [InlineData(DatabaseLogLevel.Warn)]
+    [InlineData(DatabaseLogLevel.Error)]
+    public void DefaultBehavior_Log_NothingHappens(DatabaseLogLevel level)
+    {
+        var logger = new DatabaseLogger();
+
+        logger.Log<DatabaseLoggerTests>(level, null, "This is a message");
+    }
+
+
+    [Fact]
+    public void NullLogger_NotNull()
+    {
+        var logger = DatabaseLogger.Null;
+        Assert.NotNull(logger);
+    }
+}
