e32492ab4ac1798bb41740c6ae263e59b2bd7f6a
João Simões <gravity00@users.noreply.github.com>
2025-06-09 11:47:43 +0100

    added QueryFirstOrDefaultAsync method

--- src/DapprWire/DatabaseSession.cs
+++ src/DapprWire/DatabaseSession.cs
@@ -188,6 +188,22 @@ public class DatabaseSession(
         return await connection.QueryFirstAsync<T>(command).ConfigureAwait(false);
     }
 
+    /// <inheritdoc />
+    public async Task<T?> QueryFirstOrDefaultAsync<T>(
+        string sql,
+        SqlOptions sqlOptions,
+        CancellationToken ct
+    )
+    {
+        EnsureNotDisposed();
+
+        var command = CreateCommandDefinition(sql, sqlOptions, ct);
+        LogCommandDefinition(command);
+
+        var connection = await GetDbConnectionAsync(ct).ConfigureAwait(false);
+        return await connection.QueryFirstOrDefaultAsync<T>(command).ConfigureAwait(false);
+    }
+
     /// <summary>
     /// Connects to the database asynchronously.
     /// </summary>
