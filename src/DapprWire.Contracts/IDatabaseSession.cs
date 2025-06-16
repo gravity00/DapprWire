95832cf3f64b55080f9f2889bbff659015e93006
João Simões <gravity00@users.noreply.github.com>
2025-06-16 12:07:04 +0100

    implemented databaseSession.QueryFirstOrDefault method

--- src/DapprWire.Contracts/IDatabaseSession.cs
+++ src/DapprWire.Contracts/IDatabaseSession.cs
@@ -241,6 +241,8 @@ public interface IDatabaseSession : IDisposable
 
     #endregion
 
+    #region QueryFirstOrDefault
+
     /// <summary>
     /// Executes a SQL command and returns the first result of type T.
     /// </summary>
@@ -248,12 +250,26 @@ public interface IDatabaseSession : IDisposable
     /// <param name="sql">The SQL command.</param>
     /// <param name="sqlOptions">The SQL options.</param>
     /// <param name="ct">The cancellation token.</param>
-    /// <returns>A task to be awaited for the result</returns>
+    /// <returns>A task to be awaited for the query result.</returns>
     Task<T?> QueryFirstOrDefaultAsync<T>(
         string sql,
         SqlOptions sqlOptions,
         CancellationToken ct
     );
+
+    /// <summary>
+    /// Executes a SQL command and returns the first result of type T.
+    /// </summary>
+    /// <typeparam name="T">The result type.</typeparam>
+    /// <param name="sql">The SQL command.</param>
+    /// <param name="sqlOptions">The SQL options.</param>
+    /// <returns>The query result.</returns>
+    T? QueryFirstOrDefault<T>(
+        string sql,
+        SqlOptions sqlOptions
+    );
+
+    #endregion
 
     /// <summary>
     /// Executes a SQL command and returns a grid reader for multiple result sets.
